346b3a3e941429b31838aeb2270d37588e9f23ae
kfig21 <kfig21@gmail.com>
2024-06-05 23:40:13 -0400

    Initial Commit

---
+++ tests/login.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login'
+
+test('test', async ({ page }) => {
+
+    const login = new LoginPage(page)
+
+    await login.goToLoginPage()
+    await login.login('tomsmith', 'SuperSecretPassword!')
+
+});
